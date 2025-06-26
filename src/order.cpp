160b4ca9ece3daf504ea65fb1bfecb95a5f7d57c
Colin Le <colintranle123@gmail.com>
2025-06-26 12:02:44 -0400

    start of order implementation

---
+++ src/order.cpp
@@ -0,0 +1,37 @@
+#include "order.h"
+
+namespace StockTradingSystem
+{
+    Order::Order(Trader& trader, double price, int quantity, OrderType ordertype):
+    trader_{trader},
+    price_{price},
+    quantity_{quantity},
+    timestamp_(std::time(nullptr)),
+    ordertype_{ordertype}
+    {}
+
+    Trader Order::getTrader() const
+    {
+        return trader_;
+    }
+
+    double Order::getPrice() const
+    {
+        return price_;
+    }
+
+    int Order::getQuantity() const
+    {
+        return quantity_;
+    }
+
+    std::time_t Order::getTimestamp() const
+    {
+        return timestamp_;
+    }
+
+    OrderType Order::getOrdertype() const
+    {
+        return ordertype_;
+    }
+}
