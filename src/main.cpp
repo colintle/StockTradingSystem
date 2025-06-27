30d604074a6f70f036571138aaa6dd6696be02f4
Colin Le <colintranle123@gmail.com>
2025-06-27 11:22:57 -0400

    Test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <format>
 #include "stock.h"
+#include "trader.h"
+#include "order.h"
 
 int main()
 {
     StockTradingSystem::Stock stock {145, "aapl"};
-    std::cout << "Hello World!" << std::endl;
+    StockTradingSystem::Trader trader{"Colin"};
+    StockTradingSystem::Order order {trader, 100.1, 2, StockTradingSystem::BUY};
+
+    std::string line = std::format(
+        "Stock: {}  Trader: {}  Price: {:.2f}  Qty: {}\n",
+        stock.getName(),
+        trader.getName(),
+        order.getPrice(),
+        order.getQuantity()
+    );
+    std::cout << line;
 }
