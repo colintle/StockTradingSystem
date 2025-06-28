dfba0de7274926160ca0def0a7f2d26e1cfae4ab
Colin Le <colintranle123@gmail.com>
2025-06-28 10:34:27 -0400

    implementation of orderbook

---
+++ include/orderBook.h
@@ -0,0 +1,24 @@
+#ifndef STOCK_TRADING_ORDERBOOK_H
+#define STOCK_TRADING_ORDERBOOK_H
+
+#include "order.h"
+#include "orderMatchStrategy.h"
+#include <vector>
+
+namespace StockTradingSystem
+{
+    class OrderBook
+    {
+        public:
+            OrderBook(OrderMatchingStrategy* strategy);
+            void addOrder(Order order);
+            void matchOrders();
+            void printOrderBook() const;
+            void setStrategy(OrderMatchingStrategy* strategy);
+        private:
+            std::vector<Order> book_;
+            OrderMatchingStrategy* strategy_;
+    };
+}
+
+#endif
