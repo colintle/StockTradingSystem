9921d7e0897a6a2ab7dad1950fd0ed22e935c8e8
Colin Le <colintranle123@gmail.com>
2025-06-29 13:30:47 -0400

    Support of different ordertypes in the future

---
+++ include/orderType.h
@@ -0,0 +1,12 @@
+#ifndef STOCK_TRADING_ORDER_TYPE_H
+#define STOCK_TRADING_ORDER_TYPE_H
+
+namespace StockTradingSystem
+{
+    enum class OrderType
+    {
+        GoodTillCancel
+    };
+}
+
+#endif
