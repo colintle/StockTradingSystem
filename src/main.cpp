ba7c979016e8f945ca3f18051e8ea529b974885f
Colin Le <colintranle123@gmail.com>
2025-06-30 14:26:41 -0400

    Test code for main

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,23 @@
 #include "stock.h"
 #include "trader.h"
 #include "order.h"
+#include "orderType.h"
+
+using namespace StockTradingSystem;
+
 
 int main()
 {
     StockTradingSystem::Stock stock {145, "aapl"};
     StockTradingSystem::Trader trader{"Colin"};
-    StockTradingSystem::Order order {trader, 100.1, 2, StockTradingSystem::Side::BUY};
+    StockTradingSystem::Order order {trader, 100, 2, Side::BUY, OrderType::GoodTillCancel};
 
-    std::string line = std::format(
-        "Stock: {}  Trader: {}  Price: {:.2f}  Qty: {}\n",
-        stock.getName(),
-        trader.getName(),
-        order.getPrice(),
-        order.getQuantity()
-    );
-    std::cout << line;
+    // std::string line = std::format(
+    //     "Stock: {}  Trader: {}  Price: {:.2f}  Qty: {}\n",
+    //     stock.getName(),
+    //     trader.getName(),
+    //     order.getPrice(),
+    //     order.getQuantity()
+    // );
+    // std::cout << line;
 }
