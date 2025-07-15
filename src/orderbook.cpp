7c70158b143cff90634bfab83379f415087ae0b2
Colin Le <colintranle123@gmail.com>
2025-07-15 16:08:55 -0400

    prune orders implementation

--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -249,6 +249,51 @@ namespace StockTradingSystem
 
     void Orderbook::pruneGoodForDayOrders()
     {
+        using namespace std::chrono;
+        const auto end = hours(16);
 
+        while (true)
+        {
+            const auto now = system_clock::now();
+            const auto now_c = system_clock::to_time_t(now);
+            std::tm now_parts;
+            localtime_r(&now_c, &now_parts);
+
+            if (now_parts.tm_hour >= end.count())
+                now_parts.tm_mday += 1;
+
+            now_parts.tm_hour = end.count();
+            now_parts.tm_min = 0;
+            now_parts.tm_sec = 0;
+
+            auto next = system_clock::from_time_t(mktime(&now_parts));
+            auto till = next - now + milliseconds(100);
+
+            {
+                std::unique_lock ordersLock{ ordersMutex_ };
+
+                if (shutdown_.load(std::memory_order_acquire) ||
+                    shutdownConditionVariable_.wait_for(ordersLock, till) == std::cv_status::no_timeout)
+                    return;
+            }
+
+            OrderIds orderIds;
+
+            {
+                std::scoped_lock ordersLock{ ordersMutex_ };
+
+                for (const auto& [ignored_id, entry] : orders_)
+                {
+                    const auto& [order, ignored_location] = entry;
+
+                    if (order->getOrderType() != OrderType::GoodForDay)
+                        continue;
+
+                    orderIds.push_back(order->getOrderId());
+                }
+            }
+
+            cancelOrders(orderIds);
+        }
     }
 }
